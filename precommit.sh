09b250aab3d034f8c82b2a73f6f228a49dc1392e
Alex Bradbury <asb@muxup.com>
2024-07-06 07:22:30 +0100

    [typing] Add minimal typing annotations
    There are some instances of `# type: ignore` for lines where the
    cost/benefit of making the type checker happy doesn't seem worth it.

--- precommit.sh
+++ precommit.sh
@@ -5,6 +5,7 @@ die () {
   exit 1
 }
 
+mypy --ignore-missing-imports --strict pwr || die "mypy failed"
 isort --profile black -c pwr || die "isort found issues"
 black --check pwr || die "black reports build.py needs reformatting"
 flake8 --max-line-length 88 --extend-ignore=E203,E266,E302,E501,W291 pwr || die "flake8 found issues"
